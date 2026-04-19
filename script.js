f25ec8ad6fc473e0fc9a240c2435fccaac9f03d2
Zainabu Muti <zmuti@strathmore.edu>
2026-04-19 20:52:22 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const audio = document.getElementById("audio");
+const timeDisplay = document.getElementById("time");
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function muteAudio() {
+  audio.muted = !audio.muted;
+}
+
+function setVolume(value) {
+  audio.volume = value;
+}
+
+// Update current playback time
+audio.ontimeupdate = function () {
+  timeDisplay.textContent = audio.currentTime.toFixed(1);
+};
